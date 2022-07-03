e12a8ff87b10a0aa19a9376cb2dbf592bd61597a
Luca Molinaro <luca.molinaro@students.uniroma2.eu>
2022-07-03 15:47:42 +0200

    minor changes

--- app/src/main/java/com/example/sudokump/ui/theme/Theme.kt
+++ app/src/main/java/com/example/sudokump/ui/theme/Theme.kt
@@ -8,9 +8,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = primaryGreen ,
-    secondary = textColorLight,
-    surface = lightGrey,
+    primary = primaryCharcoal,
+    secondary = textColorDark,
+    surface = lightGreyAlpha,
     primaryVariant = gridLineColorLight,
     onPrimary = accentAmber,
     onSurface = accentAmber,
@@ -22,9 +22,9 @@ private val DarkColorPalette = darkColors(
 )
 
 private val LightColorPalette = lightColors(
-    primary = primaryCharcoal,
-    secondary = textColorDark,
-    surface = lightGreyAlpha,
+    primary = primaryGreen ,
+    secondary = textColorLight,
+    surface = lightGrey,
     primaryVariant = gridLineColorLight,
     onPrimary = accentAmber,
     onSurface = accentAmber,
