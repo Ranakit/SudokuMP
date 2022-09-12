fad4a737620b3e2d0c6002867335fe45eb7ee82f
Ranakit <luca.molinaro@students.uniroma2.eu>
2022-09-12 14:13:38 +0200

    now the order should be correct

--- app/src/main/java/com/example/sudokump/viewmodel/CompletedGamesScreenViewModel.kt
+++ app/src/main/java/com/example/sudokump/viewmodel/CompletedGamesScreenViewModel.kt
@@ -7,53 +7,5 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class CompletedGamesScreenViewModel @Inject constructor(@CompletedSudokuGames val completedGames : List<SudokuGameModel>) : ViewModel(){
+class CompletedGamesScreenViewModel @Inject constructor(@CompletedSudokuGames val completedGames : List<SudokuGameModel>) : ViewModel()
 
-    fun bestTime() : Int{
-        var Index = 0
-        for(item in completedGames){
-            if (item.timePassed.compareTo(completedGames[Index].timePassed) < 0){
-                Index = completedGames.indexOf(item)
-            }
-        }
-        return Index
-    }
-
-    fun secondTime(bestTime: Int): Int {
-        var bestIndex = when{
-            bestTime == 0 -> 1
-            else -> 0
-        }
-        for(item in completedGames){
-            if (completedGames.indexOf(item) == bestTime){
-                continue
-            }
-            else{
-                if (item.timePassed.compareTo(completedGames[bestIndex].timePassed) < 0){
-                    bestIndex = completedGames.indexOf(item)
-                }
-            }
-        }
-        return bestIndex
-    }
-
-    fun thirdTime(bestTime: Int, secondTime: Int): Int {
-    var bestIndex = when{
-            bestTime + secondTime == 1 -> 2
-            bestTime == 0 -> 1
-            secondTime == 0 -> 1
-            else -> 0
-        }
-        for(item in completedGames){
-            if (completedGames.indexOf(item) == bestTime || completedGames.indexOf(item) == secondTime){
-                continue
-            }
-            else{
-                if (item.timePassed.compareTo(completedGames[bestIndex].timePassed) < 0){
-                    bestIndex = completedGames.indexOf(item)
-                }
-            }
-        }
-        return bestIndex
-    }
-}
